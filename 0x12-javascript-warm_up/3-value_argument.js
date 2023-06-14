9444016e4e71fbc96e55655f4e588f2550dd48ad
Distinctsy <oluwaseyiomi@gmail.com>
2023-06-14 13:32:03 -0700

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-const argsCount = process.argv.length - 2;
-
-if (argsCount === 0) {
-  console.log("No argument");
-} else if (argsCount === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
